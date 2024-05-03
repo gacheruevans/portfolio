6770a70129bf13891f23571034fd85abe825b9bb
Evans Gacheru <evans.gacheru.munene@gmail.com>
2024-05-03 16:57:38 +0300

    change work into projects and skills to experience

--- src/container/Work/work.jsx
+++ src/container/Work/work.jsx
@@ -9,14 +9,14 @@ import './work.scss';
 const Work = () => {
   const [activeFilter, setactiveFilter] = useState('All');
   const [animateCard, setAnimateCard] = useState({ y:0, opacity: 1});
-  const [works, setWorks] = useState([]);
-  const [filterWork, setfilterWork] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [filterProject, setfilterProject] = useState([]);
 
   useEffect(() => {
     const query = `*[_type == "works"]`;
     client.fetch(query).then((data) => {
-      setWorks(data);
-      setfilterWork(data);
+      setProjects(data);
+      setfilterProject(data);
     });
   }, [])
   
@@ -28,9 +28,9 @@ const Work = () => {
       setAnimateCard([{y:0, opacity: 1}]);
 
       if( item === 'All') {
-        setfilterWork(works);
+        setfilterProject(projects);
       }else{
-        setfilterWork(works.filter((work) => work.tags.includes(item)));
+        setfilterProject(projects.filter((project) => project.tags.includes(item)));
       }
     }, 500);
   }
@@ -53,16 +53,16 @@ const Work = () => {
         transition={{duration: 0.5, delayeChildren: 0.5}}
         className="app__work-portfolio"
      >
-        {filterWork.map((work, index) => (
+        {filterProject.map((project, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div className="app__work-img app__flex">
-              <img src={urlFor(work.imgUrl)} alt={work.name}/>
+              <img src={urlFor(project.imgUrl)} alt={project.name}/>
               <motion.div
                 whileHover={{opacity: [0, 1]}}
                 transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                <a href={project.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{scale: [0, 1]}}
                     whileHover={{scale: [1, 0.9]}}
@@ -72,7 +72,7 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                <a href={project.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{scale: [0, 1]}}
                     whileHover={{scale: [1, 0.9]}}
@@ -85,11 +85,11 @@ const Work = () => {
               </motion.div>
             </div>
             <div className="app__work-content app__flex">
-              <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{marginTop: 10}}>{work.description}</p>
+              <h4 className="bold-text">{project.title}</h4>
+              <p className="p-text" style={{marginTop: 10}}>{project.description}</p>
               
               <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
+                <p className="p-text">{project.tags[0]}</p>
               </div>
             </div>
           </div>
