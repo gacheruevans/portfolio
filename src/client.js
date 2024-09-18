b90088deee3a5b1b9281aa41d72fc72208f8a58e
Evans Gacheru <evans.gacheru.munene@gmail.com>
2024-09-18 16:45:46 +0300

    packages and sanity config updates

--- src/client.js
+++ src/client.js
@@ -6,7 +6,7 @@ export const client = sanityClient({
     dataset: 'production',
     apiVersion:'2023-03-23',
     useCdn: true,
-    token: process.env.SANITY_TOKEN,
+    token: 'skS49z7uNltPNnELH4b6UgHgs9s3enLpqpG7MpNWC5cpCR78KuQR4uMOUPIQ3BNM9L7owDt0myMGHNJ64J8m0UD0xP8mrEtka5JQPsXK6S4J8LVskU6rAIjOrxu2aSGKw0cT6jtJHPFxV52lt6pkT1MrVuXHYV5sKNzGs3mbjyYW2uniruEh',
 });
 
 const builder = ImageUrlBuilder(client);
